48c93eb706478ee742e9bc194603d3f4ddcbcffb
Alfredo Jose Sabino Ferreira <M38892@grupomadero.local>
2022-06-25 17:13:47 -0300

    Secundary Day - Event IgniteLab

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,10 @@
-import { gql, useQuery } from '@apollo/client'
-import { Header } from "./components/Header"
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-interface Lesson {
-  id: string,
-  title: string
-}
+import { Event } from './pages/Event'
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[]}>(GET_LESSONS_QUERY)
-  console.log(data)
 
   return (
     <div>
-      <Header></Header>
-
-      <ul>
-        {data?.lessons.map(lesson => {
-          return <li>{lesson.title}</li>
-        })}
-      </ul>
+      <Event></Event>
     </div>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Logo } from "./Logo";
 
 export function Header () {
     return (
-        <header className="flex justify-center bg-gray-900 border-b border-gray-500">
+        <header className="w-full py-5 flex items-center justify-center bg-gray-900 border-b border-gray-600">
             <Logo/>
         </header>
     )
